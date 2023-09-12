4266e2e00e1d001e85ed574c62ffde6a2517d920
Mykyta Kuzmytskyi <nykytakuzmytskyi@gmail.com>
2023-09-12 15:40:29 +0300

    Created OrderListView.

--- orders/views.py
+++ orders/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.views import generic
 
-# Create your views here.
+from orders.models import Order
+
+
+class OrderListView(generic.ListView):
+    model = Order
+    queryset = Order.objects.select_related("employee")
