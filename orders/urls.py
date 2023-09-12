b800ea71f6aef10063d17e02dcd11c29420bc160
Mykyta Kuzmytskyi <nykytakuzmytskyi@gmail.com>
2023-09-12 15:41:18 +0300

    Created urls for orders app.

--- orders/urls.py
+++ orders/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for producer_consumer project.
+URL configuration for Producer_Consumer project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
@@ -14,10 +14,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+
+from orders.views import OrderListView
+
+app_name = "orders"
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("orders/", include("orders.urls")),
+    path("", OrderListView.as_view(), name="order-list"),
 ]
