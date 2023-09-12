865f32b7a04511fc4df9bace33dfff41fcbead74
Mykyta Kuzmytskyi <nykytakuzmytskyi@gmail.com>
2023-09-12 15:38:21 +0300

    Created Employee & Order models.

--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,16 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class Employee(AbstractUser):
+    probation = models.DateTimeField(null=True, blank=True)
+    position = models.CharField(max_length=255)
+
+
+class Order(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    employee = models.ForeignKey(to=Employee, on_delete=models.PROTECT)
+
+    def __str__(self):
+        return f"Order:{self.pk} for {self.employee.username}"
