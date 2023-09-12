87170537794173ba365ccec54b6ec171999bfa79
Mykyta Kuzmytskyi <nykytakuzmytskyi@gmail.com>
2023-09-12 16:04:47 +0300

    Create migrations.

---
+++ orders/migrations/0002_add_data.py
@@ -0,0 +1,24 @@
+from django.contrib.auth.admin import User
+from django.conf import settings
+from django.core.management import call_command
+from django.db import migrations
+
+import orders
+
+
+def load_fixture(apps, schema_editor):
+    call_command("loaddata", "fix.json", app_label="orders")
+
+
+def reverse_func(apps, schema_editor):
+    print("reverse")
+
+
+class Migration(migrations.Migration):
+    dependencies = [
+        ("orders", "0001_initial"),
+    ]
+
+    operations = [
+        migrations.RunPython(load_fixture, reverse_func),
+    ]
